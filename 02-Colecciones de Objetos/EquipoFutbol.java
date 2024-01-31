d6c74e62f00e540bd8a4b413a3bb8872c58a9c8a
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-01-31 17:23:07 -0600

    Sesión 8 - 31/01/2024

--- 02-Colecciones de Objetos/EquipoFutbol.java
+++ 02-Colecciones de Objetos/EquipoFutbol.java
@@ -22,4 +22,33 @@ public class EquipoFutbol
         equipo = new String[11];
     }
     
+    public String getNombre() {
+        return new String(nombre);
+    }
+    
+    public String getEntrenador() {
+        return new String(entrenador);
+    }
+    
+    public void setEntrenador(String entrenador) {
+        this.entrenador = new String(entrenador);
+    }
+    
+    public void agregaJugador(int index, String jugador) {
+        if(index >= 1 && index <= 11)
+            equipo[index - 1] = new String(jugador);
+    }
+    
+    public String getJugador(int index) {
+        if(index >= 1 && index <= 11 && equipo[index - 1] != null)
+            return new String(equipo[index - 1]);
+        else
+            return null;
+    }
 }
+
+
+
+
+
+
